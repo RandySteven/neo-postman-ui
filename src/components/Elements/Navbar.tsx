88099b3ba15771abe67ea8fd8590530ba2942ee0
Randy Steven <randysteven12@gmail.com>
2024-07-23 22:58:21 +0700

    navbar

--- src/components/Elements/Navbar.tsx
+++ src/components/Elements/Navbar.tsx
@@ -4,24 +4,26 @@ import { Fragment } from "react"
 export const Navbar = () => {
     return (
         <Fragment>
-            <div className="bg-green-500 text-white py-4 px-8">
-                <div className="flex justify-between items-center">
-                    <div className="text-xl font-bold">
-                        <Link href="/">Neo-Post</Link>
+            <nav>
+                <div className="bg-green-500 text-white py-4 px-8">
+                    <div className="flex justify-between items-center">
+                        <div className="text-xl font-bold">
+                            <Link href="/">Neo-Post</Link>
+                        </div>
+                        <div className="flex space-x-4 font-bold">
+                            <Link href="/testdata/create" className="hover:bg-green-600 py-2 px-4 rounded">
+                                Test
+                            </Link>
+                            <Link href="/testdata" className="hover:bg-green-600 py-2 px-4 rounded">
+                                History
+                            </Link>
+                            <Link href="/testrecord" className="hover:bg-green-600 py-2 px-4 rounded">
+                                See Records
+                            </Link>
+                        </div>
                     </div>
-                    <div className="flex space-x-4 font-bold">
-                        <Link href="/testdata/create" className="hover:bg-green-600 py-2 px-4 rounded">
-                            Test
-                        </Link>
-                        <Link href="/testdata" className="hover:bg-green-600 py-2 px-4 rounded">
-                            History
-                        </Link>
-                        <Link href="/testrecord" className="hover:bg-green-600 py-2 px-4 rounded">
-                            See Records
-                        </Link>
-                    </div>
-                </div>
-            </div>  
+                </div>  
+            </nav>
         </Fragment>
     )
 }
